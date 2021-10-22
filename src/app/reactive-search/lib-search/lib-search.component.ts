b42fca621a230aa58b0b22b657ad0b983dbf9a21
Jairo Boaz Cavalcanti dos Santos Louzarth <jairoboaz@gmail.com>
2021-10-22 11:28:01 -0300

    Http - Aula 144

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { debounceTime, distinct, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 import { Result } from './results';
 
 @Component({
@@ -16,20 +16,37 @@ export class LibSearchComponent implements OnInit {
   queryField = new FormControl();
   results$: Observable<any>;
   total: number;
+  readonly FIELDS: string = 'name,description,version,homepag';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.results$ = this.queryField.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        filter(value => value.length > 1),
+        map(value => value.trim()),
+        //tap(value => console.log(value)),
+        switchMap(value => this.http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+      );
+      
   }
 
   onSearch(){
-    const fields = 'name,description,version,homepag';
     let value = this.queryField.value;
     if(value && (value = value.trim()) !== ''){
       
       const params = {
         search: value,
-        fields: fields
+        fields: this.FIELDS
       }
       
       this.results$ = this.http
@@ -37,6 +54,7 @@ export class LibSearchComponent implements OnInit {
       .pipe(
         tap((res: any) => this.total = res.total),
         map(res => res.results),
+        tap(console.log),
       )
     }
   }
